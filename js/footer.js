3377eb7cdff8d551ae922adf46724803e657fff8
mirkoEscobedo <escobedomirko@gmail.com>
2024-07-29 18:01:06 +0200

    [Fix] fixed light logic

--- js/footer.js
+++ js/footer.js
@@ -1,16 +1,13 @@
-const button1 = document.querySelector('#button1');
-const button2 = document.querySelector('#button2');
-const button3 = document.querySelector('#button3');
-const light_svg = document.querySelector('#light_svg');
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+const light_svg = document.getElementById('light_svg');
 light_svg.style.visibility = 'hidden';
-let light_on = false;
 
 function turnOffOn() {
-  if (light_on == false) {
-    light_on = true;
+  if (light_svg.style.visibility === 'hidden') {
     light_svg.style.visibility = 'visible';
   } else {
-    light_on = false;
     light_svg.style.visibility = 'hidden';
   }
 }
