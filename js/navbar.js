48903bb818925c195de15076ecf508aeefad9c07
noemidasta-03 <noemi.dasta18@gmail.com>
2024-09-10 17:02:29 +0200

    modifiche sidebar

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,13 @@
 const openButton = document.querySelector(".open");
 const closeButton = document.querySelector(".close");
 const sideMenuContainer = document.querySelector(`.menu-right`);
+const hamburger = document.querySelector(`#hamburger`);
 
 let isShowing = false;
 
 openButton.addEventListener(`click`, () => {
   sideMenuContainer.classList.toggle("open");
+  hamburger1.style.display = `block`;
 });
 closeButton.addEventListener(`click`, () => {
   sideMenuContainer.classList.toggle("open");
