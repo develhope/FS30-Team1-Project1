4fc568dbaae0ae6f5a897e493eae6c60656fbfe1
noemidasta-03 <noemi.dasta18@gmail.com>
2024-09-29 16:51:39 +0200

    trasform view drawer

--- js/navbar.js
+++ js/navbar.js
@@ -2,13 +2,16 @@ const openButton = document.querySelector(".open");
 const closeButton = document.querySelector(".close");
 const sideMenuContainer = document.querySelector(`.menu-right`);
 const hamburger = document.querySelector(`#hamburger`);
+const overlay = document.querySelector(".overlay");
 
 let isShowing = false;
 
 openButton.addEventListener(`click`, () => {
   sideMenuContainer.classList.toggle("open");
+  overlay.classList.toggle("active");
   hamburger1.style.display = `block`;
 });
 closeButton.addEventListener(`click`, () => {
   sideMenuContainer.classList.toggle("open");
+  overlay.classList.remove("active");
 });
