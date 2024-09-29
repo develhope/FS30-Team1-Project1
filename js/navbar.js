862dea114ea8bebf0f96f787342bfd66ab05c6bd
noemidasta-03 <noemi.dasta18@gmail.com>
2024-09-29 23:34:40 +0200

    toggle button go shopping

--- js/navbar.js
+++ js/navbar.js
@@ -4,6 +4,8 @@ const sideMenuContainer = document.querySelector(`.menu-right`);
 const hamburger = document.querySelector(`#hamburger`);
 const overlay = document.querySelector(".overlay");
 
+const closeIkea = document.querySelector(".close-Ikea");
+
 let isShowing = false;
 
 openButton.addEventListener(`click`, () => {
@@ -14,4 +16,27 @@ openButton.addEventListener(`click`, () => {
 closeButton.addEventListener(`click`, () => {
   sideMenuContainer.classList.toggle("open");
   overlay.classList.remove("active");
+});
+
+closeIkea.addEventListener(`click`, () => {
+  console.log("ciao");
+  document
+    .querySelector(".box-yellow")
+    .classList.toggle("go-shopping-menu-closed");
+  document
+    .querySelector(".box-light-yellow")
+    .classList.toggle("go-shopping-menu-closed");
+  document
+    .querySelector(".search-icon")
+    .classList.toggle("go-shopping-menu-closed");
+  document
+    .querySelector(".find-store-input")
+    .classList.toggle("go-shopping-menu-closed");
+  document
+    .querySelector(".find-store-input")
+    .classList.toggle(".find-store-input-open");
+  document
+    .querySelector("#button-list")
+    .classList.toggle("go-shopping-menu-closed");
+  document.querySelector("#button-list").classList.toggle("button-list-open");
 });
