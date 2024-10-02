9305e3ad37e2cf1dcd1ad63328c66b874f9423a4
mirkoEscobedo <escobedomirko@gmail.com>
2024-10-02 20:44:52 +0200

    carousel completed

--- js/content.js
+++ js/content.js
@@ -23,36 +23,39 @@ const carousel = document.querySelector('.carousel');
 const nextButton = document.querySelector('.next');
 const prevButton = document.querySelector('.previous');
 
+const maxScrollLeft = carousel.scrollWidth - carousel.clientWidth;
+const itemWidth = 320;
+const threshold = itemWidth * 2;
+const offset = 1;
+updateButtonVisibility();
 carousel.addEventListener('scroll', updateButtonVisibility);
 
 function updateButtonVisibility() {
-  const maxScrollLeft = carousel.scrollWidth - carousel.clientWidth;
-  const itemWidth = 320;
-  const threshold = itemWidth * 2;
-
-  if (carousel.scrollLeft >= threshold) {
+  if (carousel.scrollLeft > offset) {
     prevButton.style.display = 'flex';
   } else {
     prevButton.style.display = 'none';
   }
 
-  if (carousel.scrollLeft <= maxScrollLeft - threshold) {
+  if (carousel.scrollLeft < maxScrollLeft - offset) {
     nextButton.style.display = 'flex';
   } else {
     nextButton.style.display = 'none';
   }
 }
-updateButtonVisibility();
+
 nextButton.addEventListener('click', () => {
+  console.log('nextButton clicked');
   carousel.scrollBy({
-    left: itemWidth + 20,
+    left: itemWidth,
     behavior: 'smooth',
   });
 });
 
 prevButton.addEventListener('click', () => {
+  console.log('prevButton clicked');
   carousel.scrollBy({
-    left: -(itemWidth + 20),
+    left: -itemWidth,
     behavior: 'smooth',
   });
 });
